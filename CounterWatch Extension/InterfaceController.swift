25c8d414644e482d36a2feba05539047c63fbfbd
Miguel Hernández Jaso <miguel@mhjaso.com>
2016-11-17 21:08:33 +0100

    Add new Watch interface

---
+++ CounterWatch Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  CounterWatch Extension
+//
+//  Created by Miguel Hernández Jaso on 17/11/2016.
+//  Copyright © 2016 Miguel Hernández Jaso. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
